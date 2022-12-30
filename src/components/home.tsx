9c6e5a902a66334ee3b959eed154e972613e3d93
Keisha Zapra <kzapra@gmail.com>
2022-12-30 15:02:52 +0800

    Modify home component to have basic layout

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
+import {Options} from './options'
+import {Decider} from './decider'
+import { Logo } from './svg'
 
 // colors
 // f7fff7 white
@@ -9,18 +12,33 @@ const Background = styled.div`
     background-color: #ff6b6b;
     font-family: 'Roboto', sans-serif;
     height: 100vh;
+    padding-top: 20px;
+`;
+
+const SubBackground = styled.div`
     padding: 20px;
 `;
 
-const Title = styled.div`
-    color: #f7fff7;
-    text-align: center;
+const LogoSection = styled.div`
+    background-color: #f7fff7;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
 `
+// Fork MEAL DECISIVE Spoon
+//        ^ plate ^
 
 export const Home: FunctionComponent<{}> = () => {
     return (
         <Background>
-            <Title>Meal Decisive</Title>
+            <LogoSection>
+                <Logo width='160'/>
+            </LogoSection>
+            <SubBackground>
+                <Decider />
+                <Options />
+            </SubBackground>
         </Background>
     );
 };
